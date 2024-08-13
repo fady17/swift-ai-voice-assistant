361d27afad13a5dd119ba9edaefd6883962c4876
fady17 <fadymohamed195@gmail.com>
2024-08-13 09:35:42 +0300

    Add logic for conversation storage and user consent2

--- app/lib/icons.tsx
+++ app/lib/icons.tsx
@@ -1,38 +1,102 @@
 export function LoadingIcon() {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			className="animate-spin"
-		>
-			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-			<path d="M12 3a9 9 0 1 0 9 9" />
-		</svg>
-	);
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="animate-spin"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 3a9 9 0 1 0 9 9" />
+    </svg>
+  );
 }
 
 export function EnterIcon() {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			strokeWidth="2"
-			strokeLinecap="round"
-			strokeLinejoin="round"
-		>
-			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-			<path d="M18 6v6a3 3 0 0 1 -3 3h-10l4 -4m0 8l-4 -4" />
-		</svg>
-	);
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M18 6v6a3 3 0 0 1 -3 3h-10l4 -4m0 8l-4 -4" />
+    </svg>
+  );
+}
+export function MicrophoneIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 15a3 3 0 0 0 3 -3v-3a3 3 0 0 0 -6 0v3a3 3 0 0 0 3 3z" />
+      <path d="M5 12a7 7 0 0 1 14 0v1a7 7 0 0 1 -14 0v-1" />
+    </svg>
+  );
+}
+
+export function SettingsIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 8a4 4 0 0 1 4 4v0a4 4 0 0 1 -4 4a4 4 0 0 1 -4 -4v0a4 4 0 0 1 4 -4z" />
+      <path d="M5.5 6.5a7 7 0 0 1 13 0a7 7 0 0 1 -13 0z" />
+      <path d="M7 1v4" />
+      <path d="M17 1v4" />
+      <path d="M4 7h16" />
+      <path d="M4 17h16" />
+    </svg>
+  );
+}
+
+export function FeedbackIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M10 3h4a5 5 0 0 1 5 5v4a5 5 0 0 1 -5 5h-4a5 5 0 0 1 -5 -5v-4a5 5 0 0 1 5 -5z" />
+      <path d="M10 7v6m4 -6v6" />
+      <path d="M7 14l2 2l4 -4" />
+    </svg>
+  );
 }
